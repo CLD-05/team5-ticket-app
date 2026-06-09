fd31385703238c133fdccae5498189d6a2fd0054
dkfals8835-rgb <dkfals8835@gmail.com>
2026-06-09 13:21:13 +0900

    feat: team5-ticket-app 환경 중간 검토 및 수정 적용

--- src/main/java/com/example/ticketing/booking/repository/BookingRepository.java
+++ src/main/java/com/example/ticketing/booking/repository/BookingRepository.java
@@ -2,12 +2,10 @@ package com.example.ticketing.booking.repository;
 
 import com.example.ticketing.booking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, String> {
 
